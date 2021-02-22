94f9a080f03328b45c13ecdb918a4a4dddbf85a0
matteoaricci <matteo.a.ricci@gmail.com>
2021-02-22 12:35:49 -0500

    results display

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const SearchResults = ({results}) => {
-    return (
-        <div>
-            {results.map(result => (
-                <div>{result.author}</div>
-            ))}
+const SearchResults = ({ results }) => {
+  return (
+    <div>
+      {results.map((result) => (
+        <div
+          style={{
+            textAlign: "left",
+            margin: "10px",
+            backgroundColor: "#f5feff",
+            padding: "10px",
+          }}
+        >
+          <h2>Title: {result.title}</h2>
+          <h3>Author: {result.author}</h3>
+          <a href={result.url}>Go to Source</a>
         </div>
-    );
-}
+      ))}
+    </div>
+  );
+};
 
 export default SearchResults;
